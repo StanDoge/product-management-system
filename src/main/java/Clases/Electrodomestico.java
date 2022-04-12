0552473ea97182c0a258c25e5f6421bcd28db59e
StanDoge <StanlyDoge@gmail.com>
2022-04-11 22:04:10 -0600

    <Electrodomestico> class added

---
+++ src/main/java/Clases/Electrodomestico.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class Electrodomestico {
+    String marca;
+    int nSerie;
+    String tipo;
+    boolean estado;
+    final float IVA = 0.13f;
+    float precio;
+    float precioTotal;
+
+    //Getters & Setters
+}
