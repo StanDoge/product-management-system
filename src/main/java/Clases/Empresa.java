e7d85351e2ab8acb7768fae9c77ce183a8800f68
StanDoge <StanlyDoge@gmail.com>
2022-04-11 21:59:08 -0600

    <Empresa> class added

---
+++ src/main/java/Clases/Empresa.java
@@ -0,0 +1,13 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public class Empresa {
+    int empresaID;
+    String nombre;
+    String pais;
+    //    ArrayList<int> stock = new ArrayList<>();
+
+   //Getters & Setters
+
+}
