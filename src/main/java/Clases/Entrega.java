18cc7622e2f2962dce5571516067db80f05c264f
StanDoge <StanlyDoge@gmail.com>
2022-04-11 22:11:39 -0600

    <Entrega> class added and its getters & setters

---
+++ src/main/java/Clases/Entrega.java
@@ -0,0 +1,33 @@
+package Clases;
+
+public class Entrega {
+    int entregaID;
+    String fechaEnvio;
+    int cantidadElectrodomesticos;
+    
+    //Getters & Setters
+
+    public int getEntregaID() {
+        return entregaID;
+    }
+
+    public void setEntregaID(int entregaID) {
+        this.entregaID = entregaID;
+    }
+
+    public String getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(String fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public int getCantidadElectrodomesticos() {
+        return cantidadElectrodomesticos;
+    }
+
+    public void setCantidadElectrodomesticos(int cantidadElectrodomesticos) {
+        this.cantidadElectrodomesticos = cantidadElectrodomesticos;
+    }
+}
