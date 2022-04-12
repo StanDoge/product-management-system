33896a3707bb2042f54c3a65c740f031f91016fa
StanDoge <StanlyDoge@gmail.com>
2022-04-11 22:05:29 -0600

    <Electrodomestico>'s getters & setters added

--- src/main/java/Clases/Electrodomestico.java
+++ src/main/java/Clases/Electrodomestico.java
@@ -10,4 +10,55 @@ public class Electrodomestico {
     float precioTotal;
 
     //Getters & Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getnSerie() {
+        return nSerie;
+    }
+
+    public void setnSerie(int nSerie) {
+        this.nSerie = nSerie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public float getIVA() {
+        return IVA;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(float precioTotal) {
+        this.precioTotal = precioTotal;
+    }
 }
