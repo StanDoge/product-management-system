413bd1775297866dfce2873cf6d9e90f10df96c1
StanDoge <StanlyDoge@gmail.com>
2022-04-13 18:56:22 -0600

    Test passed, all working nice

--- src/main/java/PRN215_4_3/PRN215_4_3.java
+++ src/main/java/PRN215_4_3/PRN215_4_3.java
@@ -18,11 +18,21 @@ public class PRN215_4_3 {
         System.out.println("Hello World!");
         
         
-        Empresa generica = new Empresa(222,"generica","Colombia");
-        Electrodomestico licuadora = new Electrodomestico("generica edition",123,"Cosina", 23.83f,generica);
-        Electrodomestico[] cantidadEjemplo = {licuadora};
+        Empresa generica = new Empresa(222,"Generica","Colombia");
+        Electrodomestico licuadora = new Electrodomestico("Generica Edition",12323,"Cocina", 23.83f,generica);
+        Electrodomestico refri = new Electrodomestico("Yummy meals",132323,"Cocina", 100.40f,generica);
+        Electrodomestico cocina  = new Electrodomestico("Uwu",13323,"Cocina", 89.99f,generica);
+        Electrodomestico[] cantidadEjemplo = {licuadora,refri,cocina};
         Entrega primeraEntrega = new Entrega(11,cantidadEjemplo);
-        
-        System.out.println(generica.getNombre() + licuadora.getMarca());
+
+        /*
+          Usar String Interpolation hace mas legible  y facil al momento de imprimir data, propongo usarlo como
+          convencion en el proyecto en lugar de ir sumando uno por uno cada variable.
+
+          %s = variables de tipo stirng
+          %d = varibles de tipo numerico
+        */
+        System.out.printf("La empresa %s es duena del stock de longitud %d , lleno de productos del tipo %s%n",
+                generica.getNombre(), primeraEntrega.getCantidadElectrodomesticos(), licuadora.getTipo());
     }
 }
