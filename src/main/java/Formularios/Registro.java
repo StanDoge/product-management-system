7bbb4a0cab5e338b774d9a30c538bd268f4e49da
StanDoge <StanlyDoge@gmail.com>
2022-04-15 23:25:56 -0600

    Dependecia para tener compatibilidad con IntelliJ

--- src/main/java/Formularios/Registro.java
+++ src/main/java/Formularios/Registro.java
@@ -421,10 +421,6 @@ public class Registro extends javax.swing.JFrame {
 
 
 
-
-
-
-
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
