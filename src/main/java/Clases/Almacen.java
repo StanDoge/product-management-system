20e0c2c5cac3d10933ec16ec842ca9b7735ee0f9
StanDoge <StanlyDoge@gmail.com>
2022-04-18 19:18:00 -0600

    Creacion de <Almacen> y sus atributos encapsulados

--- src/main/java/Clases/Almacen.java
+++ src/main/java/Clases/Almacen.java
@@ -2,7 +2,7 @@ package Clases;
 
 import Interfaces.Calculos;
 
-public class Almacen {
+public class Almacen implements  Calculos{
     String nombre;
     String admin;
     int codigoPostal;
@@ -10,4 +10,54 @@ public class Almacen {
     String fechaRecibido;
     float costoAlmacenamiento;
 
+    //Constructor
+
+    //Getters & Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFechaRecibido() {
+        return fechaRecibido;
+    }
+
+    public void setFechaRecibido(String fechaRecibido) {
+        this.fechaRecibido = fechaRecibido;
+    }
+
+    public float getCostoAlmacenamiento() {
+        return costoAlmacenamiento;
+    }
+
+    public void setCostoAlmacenamiento(float costoAlmacenamiento) {
+        this.costoAlmacenamiento = costoAlmacenamiento;
+    }
 }
