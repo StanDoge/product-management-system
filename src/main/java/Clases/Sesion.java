30404813ca2b35afe25826cb7002baa25f0581aa
StanDoge <StanlyDoge@gmail.com>
2022-04-28 19:35:17 -0600

    Sesion no tiene getters ni setters

--- src/main/java/Clases/Sesion.java
+++ src/main/java/Clases/Sesion.java
@@ -4,44 +4,12 @@ import Formularios.Registro;
 
 import javax.swing.*;
 
-public class Sesion {
+public abstract class Sesion {
     String usuario;
     String pswUsuario;
     String admin;
     String pswAdmin;
 
-    //Getters & Setters
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getPswUsuario() {
-        return pswUsuario;
-    }
-
-    public void setPswUsuario(String pswUsuario) {
-        this.pswUsuario = pswUsuario;
-    }
-
-    public String getAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(String admin) {
-        this.admin = admin;
-    }
-
-    public String getPswAdmin() {
-        return pswAdmin;
-    }
-
-    public void setPswAdmin(String pswAdmin) {
-        this.pswAdmin = pswAdmin;
-    }
 
     public static boolean iniciarSesion(String us, String pswUs, String adm, String pswAdm) {
         if (us.equals("user") && pswUs.equals("sistema") || adm.equals("admin") && pswAdm.equals("PRN2")) {
